d00aaee5299f57794501c466fa84b0ab4aafa6fe
finalvalue <finalvalue@foxmail.com>
2020-09-11 15:12:29 +0800

    v0.0.8

--- main.ts
+++
@@ -1,8 +0,0 @@
-let error = false
-ps2controller.initPS2Pin(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15, DigitalPin.P16)
-basic.forever(function () {
-    error = ps2controller.readGamepad()
-    if (ps2controller.button_pressed(ps2controller.PS2Button.Up) != 0) {
-    	
-    }
-})
