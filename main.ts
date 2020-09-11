ac80a9654b731f37ebe3344009ef5e5a4146d933
finalvalue <finalvalue@foxmail.com>
2020-09-11 15:45:16 +0800

    Revert "v0.0.9"
    This reverts commit a1eefff52d503a84bf5d239b5f3e5956bfd8908e.

--- main.ts
+++
@@ -1,8 +0,0 @@
-let e = false
-ps2controller.initPS2Pin(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15, DigitalPin.P16)
-basic.forever(function () {
-    e = ps2controller.readGamepad()
-    if (ps2controller.button_pressed(ps2controller.PS2Button.Up) != 0) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-    }
-})
